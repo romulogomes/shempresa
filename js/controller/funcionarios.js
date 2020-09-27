0bc4b19b51320c22f77378a82b02ad7f74b9bf92
Romulo Gomes <romulogomes13@hotmail.com>
2020-09-27 18:20:56 -0300

    crud funcionarios

--- js/controller/funcionarios.js
+++ js/controller/funcionarios.js
@@ -1,13 +1,17 @@
 function FuncionariosCtrl ($scope, $rootScope, $http){	
-  /*
-	if(!sessionStorage.getItem('nome'))
-		location.href= "../index.html"; */
 
+  $scope.modo = "listagem";
+  
 	$scope.usuario = function(){
 		return sessionStorage.getItem('nome');
   }
 
-	$scope.lista_solicitacoes = function(){ 
+  $scope.adicionar_novo = function(){
+    $scope.nome = $scope.cpf = $scope.salario = $scope.data_de_nascimento = $scope.empregado_id = undefined;
+    $scope.modo = "edicao";
+  }
+
+	$scope.listar = function(){ 
     $scope.loading = true;
     $('#tabela').dataTable().fnClearTable();
     $('#tabela').dataTable().fnDestroy();
@@ -21,11 +25,11 @@ function FuncionariosCtrl ($scope, $rootScope, $http){
           { "width": "15%" },
           { "width": "15%" },
           { "width": "15%" },],
-      "info": false,
-      "paging" : false
+      "info": true,
+      "paging" : true,
+      "pageLength": 10,
     });
     
-
     $http({
       method: 'GET',
       url: URL+'empregados',     
@@ -38,47 +42,109 @@ function FuncionariosCtrl ($scope, $rootScope, $http){
             dado.data_de_nascimento,
             dado.cpf,
             dado.email,
-            dado.banco,
+            dado.salario,
           ]).draw());
           $scope.loading = false;
       }, function errorCallback(response) {
         alert('Não foi possivel carregar os Dados');
       });
     
-
     instancia_click();
 	}
 
   $scope.salvar = function(){
-    console.log($scope.empregado);
+    $scope.loading = true;
+    method = "";
+    dados = {
+      nome: $scope.nome,
+      data_de_nascimento: $scope.data_de_nascimento,
+      cpf: $scope.cpf,
+      salario: $scope.salario
+    }
+    if($scope.empregado_id)  {
+      dados.id = $scope.empregado_id;
+      method = $scope.atualizar(dados);
+    } else {
+      method = $scope.incluir(dados);
+    }
+    $scope.loading = false;
+  }
+
+  $scope.incluir = function(dados){
     $http({
       method: 'POST',
       url: URL+'empregados',
       headers: {'Content-Type': 'application/x-www-form-urlencoded'}, transformRequest: function(obj) {var str = [];for(var p in obj)str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p])); return str.join("&"); }, 
-      data: $scope.empregado
+      data: dados
     }).then(function successCallback(response) {
-         swal({   title: 'Cadastrado com sucesso!',   type: 'success',   timer: 2500 });
-         $scope.lista_solicitacoes();
-         $('#modal').modal('toggle');
-         setSelecionado(0);
+      swal({ title: 'Cadastrado com sucesso!', type: 'success', timer: 2500 });
+      $scope.listar();
+      setSelecionado(0);
+      $scope.modo = "listagem";
+      $scope.loading = false;
+    }, function errorCallback(response) {
+      alert('Erro '+ response.data);
+    });
+  }
+
+  $scope.atualizar = function(dados){
+    $http({
+      method: 'PUT',
+      url: URL+'empregados/'+dados.id,
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'}, transformRequest: function(obj) {var str = [];for(var p in obj)str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p])); return str.join("&"); }, 
+      data: dados
+    }).then(function successCallback(response) {
+      swal({ title: 'Atualizado com sucesso!', type: 'success', timer: 2500 });
+      $scope.listar();
+      $scope.modo = "listagem";
+      setSelecionado(0);
+      $scope.loading = false;
+    }, function errorCallback(response) {
+      console.log('erro');
+    });
+  }
+
+  $scope.cancelar = function(){
+    $scope.modo = "listagem";
+  }
+
+  $scope.monta_edit = function(){
+    $scope.loading = true;
+    var table = $('#tabela').DataTable();
+    var dados = table.rows('.selected').data();
+    $http({
+      method: 'GET',
+      url: URL+'empregados/'+dados[0][0],     
+    }).then(function successCallback(response) {
+        console.log(response.data);
+        dados = response.data
+        $scope.empregado_id = dados.id;
+        $scope.nome = dados.nome;
+        $scope.cpf = dados.cpf;
+        $scope.salario = dados.salario;
+        $scope.loading = false;
+        $scope.modo = "edicao";
       }, function errorCallback(response) {
-        console.log('erro');
+        alert('Não foi possivel carregar os Dados');
       });
+
   }
 
   $scope.excluir = function(){
-    swal({   title: 'Excluir?',   text: 'Você tem certeza que deseja excluir?',  type: 'warning', cancelButtonText: 'Cancelar',  showCancelButton: true,   confirmButtonColor: '#3085d6',   cancelButtonColor: '#d33',   confirmButtonText: 'Sim, Excluir!',   closeOnConfirm: false }, function() {  
-  
+    swal({title: 'Excluir?', text: 'Você tem certeza que deseja excluir?', type: 'warning', cancelButtonText: 'Cancelar', showCancelButton: true, confirmButtonColor: '#3085d6', cancelButtonColor: '#d33', confirmButtonText: 'Sim, Excluir!', closeOnConfirm: false }, function() {  
     var table = $('#tabela').DataTable();
     var dados = table.rows('.selected').data();
+    $scope.loading = true;
     $http({
       method: 'DELETE',
       url: URL+'empregados/'+dados[0][0],
     }).then(function successCallback(response) {
-        swal({   title: 'Excluido com Sucesso',   type: 'success',   timer: 2500 });
-        $scope.lista_solicitacoes();
+        swal({ title: 'Excluído com Sucesso', type: 'success', timer: 2500 });
+        $scope.listar();
+        $scope.loading = false;
       }, function errorCallback(response) {
         alert('erro' + JSON.stringify(response.data));
+        $scope.loading = false;
       });
     });
   }
