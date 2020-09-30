05ba1245759a49d450a1b48f92aeded688001795
Romulo Gomes <romulogomes13@hotmail.com>
2020-09-30 20:14:46 -0300

    aponta para o servidor

--- js/env/env.js
+++ js/env/env.js
@@ -1,2 +1,2 @@
-//  const URL = "https://sheltered-reef-35315.herokuapp.com/";
-const URL = "http://127.0.0.1:3000/";
+const URL = "https://sheltered-reef-35315.herokuapp.com/";
+//const URL = "http://127.0.0.1:3000/";
