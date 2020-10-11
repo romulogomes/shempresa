631dd00e0708b42d63729c868f4862f91ef7bf24
Romulo <romulogomes13@hotmail.com>
2020-10-11 12:11:36 -0300

    adiciona filtro de empresa nas requisições

--- login/js/controller.js
+++ login/js/controller.js
@@ -2,7 +2,8 @@ function meucontroller($scope, $http){
 	
 	$scope.envia = function(){
 		if($scope.usuario == "farmaciadojauro" && $scope.senha == "jesus"){
-			sessionStorage.setItem('nome' , 'Farmácia do Jauro');
+			sessionStorage.setItem('nome', 'Farmácia do Jauro');
+			sessionStorage.setItem('empresa_id', 1);
 			location.href="../solicitacoes.html";
 		} else {
 			alert("Usuário e/ou Senha incorreta");
