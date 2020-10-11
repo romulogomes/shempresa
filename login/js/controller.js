090991fb7135703d88488e07040f9163eed4c17f
Romulo <romulogomes13@hotmail.com>
2020-10-11 15:58:58 -0300

    empresa de teste

--- login/js/controller.js
+++ login/js/controller.js
@@ -5,6 +5,10 @@ function meucontroller($scope, $http){
 			sessionStorage.setItem('nome', 'Farmácia do Jauro');
 			sessionStorage.setItem('empresa_id', 1);
 			location.href="../solicitacoes.html";
+		} else if ($scope.usuario == "empresa_teste" && $scope.senha == "jesus") {
+			sessionStorage.setItem('nome', 'Empresa de Testes');
+			sessionStorage.setItem('empresa_id', 2);
+			location.href="../solicitacoes.html";
 		} else {
 			alert("Usuário e/ou Senha incorreta");
 		}
